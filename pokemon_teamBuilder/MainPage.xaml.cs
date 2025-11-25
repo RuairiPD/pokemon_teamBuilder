c44966c7b12d7e47045bb2e6208db3e4758ba424
RuairiPfetzingDonnel <polomints2@gmail.com>
2025-11-25 14:57:41 +0000

    Small text change

--- pokemon_teamBuilder/MainPage.xaml.cs
+++ pokemon_teamBuilder/MainPage.xaml.cs
@@ -1,9 +1,10 @@
-﻿namespace pokemon_teamBuilder
+﻿using System.Text.Json;
+using Microsoft.Maui.Storage;
+
+namespace pokemon_teamBuilder
 {
     public partial class MainPage : ContentPage
     {
-        
-
         public MainPage()
         {
             InitializeComponent();
